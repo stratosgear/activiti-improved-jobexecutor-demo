0345147200294093a74be15dd87f72df21c3d93b
Stratos Gerakakis <stratosgear@gmail.com>
2014-11-19 09:55:20 +0100

    Added debug statements from SleepDelegate

--- src/main/java/org/activiti/SleepDelegate.java
+++ src/main/java/org/activiti/SleepDelegate.java
@@ -1,6 +1,5 @@
 package org.activiti;
 
-
 import java.util.Date;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -9,23 +8,24 @@ import org.activiti.engine.delegate.Expression;
 import org.activiti.engine.delegate.JavaDelegate;
 
 public class SleepDelegate implements JavaDelegate {
-	
-	public static AtomicInteger nrOfExecutions = new AtomicInteger(0);
 
-  protected Expression sleepTime;
+    public static AtomicInteger nrOfExecutions = new AtomicInteger(0);
 
-  public void execute(DelegateExecution execution) throws Exception {
-  	long startTime = System.currentTimeMillis();
-  	long var = 0;
-  	while (System.currentTimeMillis() - startTime < 200) {
-  		var += startTime; // Doing something to keep the JVM busy
-  		Thread.sleep(50L); // Doing some sleeps to mimic I/O
-  	}
-  	nrOfExecutions.incrementAndGet();
-//  	System.out.println(new Date() + "Job Done!");
-  }
+    protected Expression sleepTime;
 
-  public void setSleepTime(Expression sleepTime) {
-    this.sleepTime = sleepTime;
-  }
+    public void execute(DelegateExecution execution) throws Exception {
+        long startTime = System.currentTimeMillis();
+        long var = 0;
+        while (System.currentTimeMillis() - startTime < 200) {
+            var += startTime; // Doing something to keep the JVM busy
+            Thread.sleep(50L); // Doing some sleeps to mimic I/O
+        }
+        nrOfExecutions.incrementAndGet();
+        System.out.println(String.format("%d: %s Job Done after sleeping for %s millis",
+                nrOfExecutions.get(), new Date(), this.sleepTime.getExpressionText()));
+    }
+
+    public void setSleepTime(Expression sleepTime) {
+        this.sleepTime = sleepTime;
+    }
 }
